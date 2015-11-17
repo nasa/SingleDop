49a8120110433319a65c9ee7d6b0747477409de1
Joseph C. Hardin <joseph.hardin@pnnl.gov>
2015-11-17 13:40:12 -0800

    Updated version, imports, removed extraneous comment

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup
 
 setup(
     name='SingleDop',
-    version='0.5',
+    version='0.8',
     author='Timothy Lang',
     author_email='timothy.j.lang@nasa.gov',
     packages=['singledop',],
-    #url='http://pypi.python.org/pypi/PyDisdrometer/',
     license='LICENSE.txt',
     description='Single Doppler Retrieval Toolkit',
     long_description=open('description.txt').read(),
